a70bf8abd3174e9c7dbfb399b782c368e40f4063
TheFirstAvenger <mike@mikebinns.net>
2019-04-12 12:48:01 -0400

    Gigalixir changes.

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :logic_sim_liveview, LogicSimLiveviewWeb.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "logic-sim.herokuapp.com", port: 443],
+  url: [scheme: "https", host: Map.fetch!(System.get_env(), "APP_HOSTNAME"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
